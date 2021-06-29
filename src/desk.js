77769f3b3f1fbc554895a856107b527d2a34ed07
Matthew Yorke <mattdyorke@gmail.com>
2021-06-29 15:27:59 -0400

    wrote computer, desk, marker classes

--- src/desk.js
+++ src/desk.js
@@ -1,5 +1,20 @@
 class Desk {
-  
+  constructor(length, width, height, isWhiteboard){
+      this.length = length;
+      this.width = width;
+      this.height = height;
+      this.isWhiteBoard = isWhiteboard;
+      this.content = '';
+  }
+
+  write(stuff){
+    content = content + stuff;
+  }
+
+  wipe(){
+    if(isWhiteboard)
+        content = '';
+  }
 }
 
 module.exports = Desk
--- src/marker.js
+++ src/marker.js
@@ -1,5 +1,27 @@
 class Marker {
-  
+  constructor(size, color, remainingInk){
+    this.size = size;
+    this.color = color;
+    this.remainingInk = remainingInk
+  }
+
+  write(stuff){
+    let stuffWrote = '';
+    let words = stuff.split('');
+    
+    for(var char in words){
+        if(char === ' '){
+            stuffWrote = stuffWrote + char;
+            continue;
+        }else if(this.remainingInk >= 1){
+            remainingInk --;
+            stuffWrote = stuffWrote + char;
+        }else{
+            break;
+        }
+    }
+    return stuffWrote;
+  }
 }
 
 module.exports = Marker
